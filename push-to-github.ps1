4f24addb919ef5cc8ae679b33ed68dd026bfe7cb
wekasoko-joe-wa-mwangi-2026 <wekasoko@gmail.com>
2026-05-04 13:06:02 +0300

    Add push script

---
+++ push-to-github.ps1
@@ -0,0 +1,5 @@
+# Push to GitHub
+cd C:\Users\Bryan\Documents\WEKA SOKO\weka-soko-backend
+$gitExe = "C:\Users\Bryan\AppData\Local\GitHubDesktop\app-3.5.8\resources\app\git\cmd\git.exe"
+& $gitExe push -u origin main
+Write-Host "Push completed!"
